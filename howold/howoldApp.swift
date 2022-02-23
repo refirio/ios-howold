d61864dfb28b3718b5904096fcd8572aa787823f
refirio <refirio@gmail.com>
2022-02-23 15:08:21 +0900

    Initial Commit

---
+++ howold/howoldApp.swift
@@ -0,0 +1,17 @@
+//
+//  howoldApp.swift
+//  howold
+//
+//  Created by refirio.
+//
+
+import SwiftUI
+
+@main
+struct howoldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
